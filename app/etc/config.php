90fc24777e1b91316917974d5b4f37a331ec1d89
Iurii Stepanenko <uzik.fk11@gmail.com>
2019-11-21 23:29:31 +0200

    Added a new CMS blocks && added a new block with template to the Product Page && Modified the newsletter subscription template && Created a new database dump

--- app/etc/config.php
+++ app/etc/config.php
@@ -238,6 +238,7 @@ return [
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
         'Dotdigitalgroup_Email' => 1,
+        'Iuriis_Cms' => 1,
         'Iuriis_LayoutDebug' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
