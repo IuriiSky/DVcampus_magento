b19469b3e7efb72e9b2874289d3cfb954400a7d0
IuriiSky <uzik.fk11@gmail.com>
2020-01-27 18:36:14 +0200

    Created new cache type for private conten

---
+++ app/code/Iuriis/Chatbox/CustomerData/CustomerMessages.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Iuriis\Chatbox\CustomerData;
+
+class CustomerMessages implements \Magento\Customer\CustomerData\SectionSourceInterface
+{
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getSectionData()
+    {
+        // TODO: Implement getSectionData() method.
+    }
+}
