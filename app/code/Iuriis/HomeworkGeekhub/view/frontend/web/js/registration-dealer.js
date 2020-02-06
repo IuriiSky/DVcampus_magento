49dbd99121275f7d34e39a972e70dd5aff9177d0
IuriiSky <uzik.fk11@gmail.com>
2020-02-06 18:36:09 +0200

    Created modal window from form registration

--- app/code/Iuriis/HomeworkGeekhub/view/frontend/web/js/registration-dealer.js
+++ app/code/Iuriis/HomeworkGeekhub/view/frontend/web/js/registration-dealer.js
@@ -1,26 +1,52 @@
 define([
     'jquery',
-    'jquery/ui',
     'Magento_Ui/js/modal/alert',
+    'Magento_Ui/js/modal/modal',
+    'jquery/ui',
     'mage/translate'
 ], function ($) {
     'use strict';
+
     $.widget('iuriisHomework.registrationDealer', {
 
         /**
          * @private
          */
         _create: function () {
-            $(this.element).on('click.iuriis_homework', $.proxy(this.openModal, this));
+            $(document).on('click.iuriis_homework', $.proxy(this.openModalWindow, this));
+            $(document).on('click.iuriis_homework', $.proxy(this.changeId, this));
+            this.modal = $(this.element);
+
         },
 
         _destroy: function () {
-            $(this.element).off('click.iuriis_homework');
+            $(document).off('click.iuriis_homework');
         },
 
-        openModal: function () {
-            alert('Hello!');
+        openModalWindow: function () {
+            $('#registration-dealer-btn').on('click', function () {
+                $('#form-validate-dealer').modal({
+                    buttons: [],
+                }).modal('openModal');
+            })
+        },
+
+        changeId: function () {
+            $('#form-validate-dealer').find("[id]").each(function () {
+                this.id = this.id + '-dealer';
+            })
         }
+
     });
-return $.iuriisHomework.registrationDealer;
+    return $.iuriisHomework.registrationDealer;
 });
+
+
+// var myFeatured = $('div.featured').attr('id');
+// var theHidden;
+// $('div.article').each(function(){
+//     theHidden = $(this).attr('id');
+//     if(theHidden == myFeatured){
+//         $('div#'+theHidden).hide();
+//     }
+// });
