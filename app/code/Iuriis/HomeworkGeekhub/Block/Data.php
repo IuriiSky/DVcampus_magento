a730d733f4972b4a1ad2c9f06dff4cd475848ce9
IuriiSky <uzik.fk11@gmail.com>
2020-03-11 17:56:51 +0200

    Homework geekhub dealer-form registration

--- app/code/Iuriis/HomeworkGeekhub/Block/Data.php
+++ app/code/Iuriis/HomeworkGeekhub/Block/Data.php
@@ -1,10 +1,19 @@
 <?php
+declare(strict_types=1);
 
+namespace Iuriis\HomeworkGeekhub\Block;
 
-namespace Iuriis\HomeworkGeekhub\Block\Widget;
-
-
-class Data
+class Data extends \Magento\Framework\View\Element\Html\Select
 {
-
+    /**
+     * Set element's HTML ID
+     *
+     * @param string $elementId ID
+     * @return $this
+     */
+    public function setId($elementId)
+    {
+        $this->setData('id', $elementId);
+        return $this;
+    }
 }
